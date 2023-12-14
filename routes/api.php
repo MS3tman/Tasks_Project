a94c44e6cd57bf85318951dde9b4e60a9355086e
MS3tman <3tmancs00@gmail.com>
2023-12-14 16:01:40 +0200

    create APIs

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+route::get('/tasks',[TaskController::class, 'index']);
+
+route::get('/tasks/{id}',[TaskController::class, 'show']);
